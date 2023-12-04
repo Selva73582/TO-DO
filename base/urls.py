3ea681f612b9348478c4bdefd7331ea3ef0bf9b8
Selva73582 <102418337+Selva73582@users.noreply.github.com>
2023-12-04 22:34:43 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -6,10 +6,11 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('',views.home),
     path('login/',views.CustomLogin.as_view(),name="login"),
     path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.RegisterPage.as_view(), name='register'),
-    path('',views.TaskList.as_view(),name="TaskList"),
+    path('TaskList',views.TaskList.as_view(),name="TaskList"),
     path('task/<int:pk>/',views.TaskDetail.as_view(),name="Task-View"),
     path('task-create/',views.TaskCreate.as_view(),name="Task-Create"),
     path('task-edit/<int:pk>/',views.TaskUpdate.as_view(),name="Task-Edit"),
